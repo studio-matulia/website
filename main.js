a5332cfc64e52035bdaa344209e57815458ba36d
matthiasmeissen <meissenmatthias@gmail.com>
2025-08-08 19:41:13 +0200

    Set up basic page

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-
+const theme_button = document.getElementById("theme-button");
 let isDarkTheme = false;
 
 document.addEventListener('keydown', function(e) {
     if (e.key === 't') {
-        isDarkTheme = !isDarkTheme;
-        document.body.classList.toggle('dark', isDarkTheme);
+        toggleTheme();
     }
 });
+
+theme_button.addEventListener("click", toggleTheme)
+
+function toggleTheme() {
+    isDarkTheme = !isDarkTheme;
+    document.body.classList.toggle('dark', isDarkTheme);
+}
