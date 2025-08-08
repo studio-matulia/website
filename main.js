1d15d785f4e66a655f6a565a2214d3b379c92e34
matthiasmeissen <meissenmatthias@gmail.com>
2025-08-08 19:44:14 +0200

    Add github link

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const theme_button = document.getElementById("theme-button");
+const github_button = document.getElementById("github-button");
 let isDarkTheme = false;
 
-document.addEventListener('keydown', function(e) {
-    if (e.key === 't') {
-        toggleTheme();
-    }
-});
-
-theme_button.addEventListener("click", toggleTheme)
+// --- Functions ---
 
 function toggleTheme() {
     isDarkTheme = !isDarkTheme;
     document.body.classList.toggle('dark', isDarkTheme);
 }
+
+function openGitHub() {
+    window.open('https://github.com/studio-matulia', '_blank');
+}
+
+// --- Event Listeners ---
+
+// Listen for keyboard shortcuts
+document.addEventListener('keydown', function(e) {
+    if (e.key === 't') {
+        toggleTheme();
+    } else if (e.key === 'g') {
+        openGitHub();
+    }
+});
+
+// Listen for button clicks
+theme_button.addEventListener("click", toggleTheme);
+github_button.addEventListener("click", openGitHub);
