9189e6b600287555e6c08b63f6c04322c98ea70f
matthiasmeissen <meissenmatthias@gmail.com>
2025-08-08 20:09:41 +0200

    Add loading glyphs

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const theme_button = document.getElementById("theme-button");
 const github_button = document.getElementById("github-button");
 let isDarkTheme = false;
 
+
 // --- Functions ---
 
 function toggleTheme() {
@@ -12,6 +13,7 @@ function toggleTheme() {
 function openGitHub() {
     window.open('https://github.com/studio-matulia', '_blank');
 }
+
 
 // --- Event Listeners ---
 
@@ -27,3 +29,22 @@ document.addEventListener('keydown', function(e) {
 // Listen for button clicks
 theme_button.addEventListener("click", toggleTheme);
 github_button.addEventListener("click", openGitHub);
+
+
+// --- Loading Glyph Animation ---
+
+const loadingElement = document.getElementById('loading-glyph');
+
+if (loadingElement) {
+    const GLYPHS = ['╱', '╌', '╲', '╎', '╱', '╌', '╲', '╎', ' ', '░', '▒', '▓', '▒', '░', ' '];
+
+    const ANIMATION_SPEED_MS = 400;
+
+    let currentGlyphIndex = 0;
+
+    function animateGlyph() {
+        loadingElement.textContent = GLYPHS[currentGlyphIndex];
+        currentGlyphIndex = (currentGlyphIndex + 1) % GLYPHS.length;
+    }
+    setInterval(animateGlyph, ANIMATION_SPEED_MS);
+}
